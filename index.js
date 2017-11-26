861c6550e5fb3e25972f079c5e01f004b079a443
helabenkhalfallah <hela.bk@oyez.fr>
2017-11-26 14:20:41 +0100

    sample js cli sonar configuration

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 
 
+var Car = function(brand, model, year) {
+	this.brand = brand;
+	this.model = model;
+	this.year = year;
+	this.engineState = 'stopped';
+	this.messageToDriver = '';
+};
 
-console.log('hellxxxxxsssssso');
+let aCar = new Car("Volks Wogen","Polo 6","2017");
+console.log('aCar : ', aCar);
+
+//prepare array
+var myArray = ['a', 'b', 'c', 'd'];
+console.log("item to delete : ",myArray[2]);
+
+// removes 1 element from index 2
+var removed = myArray.splice(2, 1);
+console.log("deleted item : ", removed);
